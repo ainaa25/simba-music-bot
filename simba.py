f9594869600bdcc96e8a82ab349ab11e20749231
ainaa25 <ainainsyirah92@gmail.com>
2025-06-19 18:58:09 +0800

    Update simba.py

--- simba.py
+++ simba.py
@@ -3,10 +3,12 @@ import os
 
 simba = Client(
     "simba",
-    api_id = 
-int(os.environ["API_ID"]),
-    api_hash =
-os.environ["API_HASH"],
-    bot_token =
-os.environ["BOT_TOKEN"]
+    api_id = int(os.environ["API_ID"]
+    ),
+    
+    api_hash =os.environ["API_HASH"]
+    ,
+    
+    bot_token =os.environ["BOT_TOKEN
+    "]
 )
