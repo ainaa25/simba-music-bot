bfca600c0a347acd706f74898e62be08f4b11cf9
ainaa25 <ainainsyirah92@gmail.com>
2025-06-19 18:20:28 +0800

    Create simba.py

---
+++ simba.py
@@ -0,0 +1,9 @@
+from pyrogram import Client
+import os
+
+simba = Client(
+    "simba",
+    api_id=int(os.environ["API_ID"]),
+    api_hash=os.environ["API_HASH"]),
+    bot_token=os.environ["BOT_TOKEN"]
+)
